88a45d45ad368161278e4607d66cd2a25217f93a
Govind R Nair <govindrnair.rev@gmail.com>
2023-05-23 20:20:38 +0530

    path and queries

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Optional
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World!"}
+inventory = {
+    1: {
+        "name": "Bread",
+        "price": 24.99,
+        "type": "Wheat"
+    }
+}
 
+
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int = Path(description="The ID")):
+    return inventory[item_id]
+
+
+@app.get("/get-by-name")
+def get_item(*, name: Optional[str] = None):
+    for item_id in inventory:
+        if inventory[item_id]["name"] == name:
+            return inventory[item_id]
+    return {"Data": "Not found"}
