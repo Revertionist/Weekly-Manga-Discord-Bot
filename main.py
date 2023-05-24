e25801970300dc0f431cb90d679b51f606947753
Govind R Nair <govindrnair.rev@gmail.com>
2023-05-24 12:50:05 +0530

    post method

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI, Path
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+    brand: Optional[str] = None
+
 inventory = {
-    1: {
-        "name": "Bread",
-        "price": 24.99,
-        "type": "Wheat"
-    }
+    
 }
 
 
@@ -20,6 +22,13 @@ def get_item(item_id: int = Path(description="The ID")):
 @app.get("/get-by-name")
 def get_item(*, name: Optional[str] = None):
     for item_id in inventory:
-        if inventory[item_id]["name"] == name:
+        if inventory[item_id].name == name:
             return inventory[item_id]
     return {"Data": "Not found"}
+
+@app.post("/create-item/{item_id}")
+def create_item(item_id: int, item: Item):
+    if item_id in inventory:
+        return {"Error": "Item ID alreayd exists"}
+    inventory[item_id] = item
+    return inventory[item_id]
