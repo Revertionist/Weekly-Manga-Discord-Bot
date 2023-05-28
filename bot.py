291f8f440de47289713271b2a05b6a8188018dc6
Govind R Nair <govindrnair.rev@gmail.com>
2023-05-28 19:00:28 +0530

    Bot now working

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 import aiohttp
 import os
 import discord
-from discord import app_commands
+from discord import app_commands, Embed, Color
+
 
 load_dotenv()
 
@@ -12,7 +13,6 @@ MY_GUILD = discord.Object(id=932674444583911474)  # replace with your guild id
 
 class MyClient(discord.Client):
     def __init__(self, *, intents: discord.Intents):
-        self.http_client = aiohttp.ClientSession()
         super().__init__(intents=intents)
         # A CommandTree is a special type that holds all the application command
         # state required to make it work. This is a separate class because it
@@ -52,9 +52,22 @@ async def hello(interaction: discord.Interaction):
 async def data_list(interaction: discord.Interaction):
     """Says hello!"""
     data = None
-    async with client.http_client.get('http://127.0.0.1:8000') as req:
-      data = await req.json()
-      print (data)
-    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
+    embeds = []
+    # embed = Embed(title="Manga", description="this is desc", color=Color.red())
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get('https://weeklymangaapi--revertionist.repl.co/') as resp:
+            data = await resp.json()
+            print (data)
+    for i in data:
+        embed = Embed(title=i["title"], description=i["chapter"], url=i["link"], color=Color.blurple())
+        link = i["link"]
+        embed.add_field(name="Link", value=f"[open]({link})")
+        embeds.append(embed)
+
+        # embed.add_field(name=i["title"], value=i["chapter"])
+    # async with client.http_client.get('https://weeklymangaapi--revertionist.repl.co/') as req:
+    #   data = await req.json()
+    #   print (data)
+    await interaction.response.send_message(embeds=embeds)
 
 client.run(os.environ.get("DISCORD_TOKEN"))
